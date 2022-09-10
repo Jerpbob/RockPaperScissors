20da15c650a79430f3feff8430148933c6c2ed4e
Jeru Balares <jerupaulbalares@gmail.com>
2022-09-09 19:09:20 -0700

    Added a feature that showcases the current scores of the user and the computer

--- scripts.js
+++ scripts.js
@@ -11,17 +11,43 @@ function playRound(playerSelection, computerChoice) {
     console.log(playerSelection)
     console.log(computerChoice)
     if (playerSelection == computerChoice) {
-        console.log('It\'s a tie!');
+        return "tie";
     } else if (((playerSelection == "rock") && (computerChoice == "paper")) ||
         ((playerSelection == "paper") && (computerChoice == "scissors")) ||
         ((playerSelection == "scissors") && (computerChoice == "rock"))) {
         console.log(`It\'s your ${playerSelection} versus the computer\'s ${computerChoice}, the computer wins!`);
-        computerScore++
+        computerScore++;
+        return "computer"
     } else if (((playerSelection == "paper") && (computerChoice == "rock")) ||
         ((playerSelection == "scissors") && (computerChoice == "paper")) ||
         ((playerSelection == "rock") && (computerChoice == "scissors"))) {
         console.log(`It\'s your ${playerSelection} versus the computer\'s ${computerChoice}, you win!`);
-        yourScore++
+        yourScore++;
+        return "user"
     }
 };
+
+
+const results = document.getElementById("results");
+const yScore = document.querySelector(".yScore");
+const cScore = document.querySelector(".cScore");
+
+window.addEventListener('click', function (e) {
+    let computerChoice = getComputerChoice();
+    let result = playRound(e.target.id, computerChoice);
+    console.log(yScore);
+    console.log(cScore);
+    if (result === "tie") {
+        results.innerHTML = "Tie!";
+    } else if (result === "computer") {
+        results.innerHTML = "Computer wins!";
+        cScore.textContent = computerScore;
+    } else if (result === "user") {
+        results.innerHTML = "You win!";
+        yScore.textContent = yourScore;
+    } else {
+        results.innerHTML = "There's something wrong";
+    }
+
+});
 
